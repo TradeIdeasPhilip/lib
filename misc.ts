d69b8aa82f9bc2f88b9e2a6f5bc00a9da23903fb
Philip Smolen <philip+github@trade-ideas.com>
2022-04-22 00:58:23 -0700

    Add missing functions.
    I'm consolidating from different versions of misc.ts.

--- misc.ts
+++ misc.ts
@@ -159,3 +159,49 @@ export const NON_BREAKING_SPACE = "\xa0";
  * Looks like a space.  Is the width of a digit.
  */
 export const FIGURE_SPACE = "\u2007";
+
+// https://dev.to/chrismilson/zip-iterator-in-typescript-ldm
+type Iterableify<T> = { [K in keyof T]: Iterable<T[K]> };
+/**
+ * Given a list of iterables, make a single iterable.
+ * The resulting iterable will contain arrays.
+ * The first entry in the output will contain the first entry in each of the inputs.
+ * The nth entry in the output will contain the nth entry in each of the inputs.
+ * This will stop iterating when the first of the inputs runs out of data.
+ * ```
+ *   for (const [rowHeader, rowBody] of zip(sharedStuff.rowHeaders, thisTable.rowBodies)) {
+ *     ...
+ *   }
+ * ```
+ * @param toZip Any number of iterables.
+ */
+export function* zip<T extends Array<any>>(
+  ...toZip: Iterableify<T>
+): Generator<T> {
+  // Get iterators for all of the iterables.
+  const iterators = toZip.map((i) => i[Symbol.iterator]());
+
+  while (true) {
+    // Advance all of the iterators.
+    const results = iterators.map((i) => i.next());
+
+    // If any of the iterators are done, we should stop.
+    if (results.some(({ done }) => done)) {
+      break;
+    }
+
+    // We can assert the yield type, since we know none
+    // of the iterators are done.
+    yield results.map(({ value }) => value) as T;
+  }
+}
+
+export function* count(start = 0, end = Infinity, step = 1) {
+  for (let i = start; i < end; i += step) {
+    yield i;
+  }
+}
+
+export function sum(items: number[]): number {
+  return items.reduce((accumulator, current) => accumulator + current, 0);
+}
